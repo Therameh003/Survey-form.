f17948726d8832283655416da68697405f1b3110
Ramesh Muthuraman <113369770+Therameh003@users.noreply.github.com>
2024-02-23 05:17:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+print(" CORONA AFFECTED COUNTRIES IN THE WORLD".center(150))
+def my_function(a,b,c):
+    print("Total cases of CORONA="+str(a))
+    print("Recover              ="+str(b))
+    print("Death                ="+str(c))
+    d=b/a*100
+    print("Recover percentage % ="+str(d)+"%")
+    e=c/a*100
+    print("Death percentage   % ="+str(e)+"%")
+    f=b+c-a
+    print("Active Cases="+str(f) +"  peoples....")
+print("COVID-19   world".center(30))
+my_function(657082848,631103118,6670036)
+print(" CORONA AFFECTED COUNTRIES IN THE WORLD".center(150))
+country=input("ENTER COUNTRY NAME:")
+print(country.center(150))
+
+if country=='india' or country=='INDIA':
+    print("Name Of The Country INDIA")
+    my_function(44676911,44141501,530667)
+elif country=='united states' or country=='usa' or country=='USA' or country=='UNITED STATES' or country=='unitedstates':
+    print("Name Of The Country USA")
+    my_function(101743845,98803214,1112944)
+elif country=='france' or country=='FARNCE':
+    print("Name Of The Country FRANCE")
+    my_function(38801884,15867601,160359)
+
+elif country=='genmany' or country=='GERMANY':
+    print("Name Of The Country GERMANY")
+    my_function(36980882,36229900,159884)
+elif country == 'brazil' or country=='BRAZIL':
+    print("Name Of The Country BRAZIL")
+    my_function(35874528,34542868,691776)
+elif country=='korea' or country=='KOREA':
+    print("name of the country ")
+    my_function(28188293,27067381,31395)
+elif country == 'japan' or country=='JAPAN':
+    print("name of the country JAPAN")
+    my_function(27116473,20921895,53319)
+elif country=='italy' or country=='ITALY':
+    print("name of the country ITALY")
+    my_function(24884034,24215242,183138)
+elif country=='uk' or country=='UK' or country=='united kingdom' or country=='UNITED KINGDOM':
+    print("name of the country UK")
+    my_function(24089042,23794739,198271)
+elif country=='russia' or country=='RUSSIA':
+    print("name of the country RUSSIA")
+    my_function(21716074,21109668,393000)
+elif country=='spain' or country=='SPAIN':
+    print("name of the country SPAIN ")
+    my_function(13651239,13444388,116658)
+elif country=='vietnam' or country=='VIETNAM':
+    print("name of the country VIETNAM ")
+    my_function(11522927,10610266,43179)
+elif country=='austraila' or country=='AUSTRAILA':
+    print("name of the country  AUSTRALIA")>
+    my_function(10972385,10546102,16692)
+elif country=='argentina' or country=='ARGENTINA':
+    print("name of the country ARDENTINA")
+    my_function(9766975,9593883,130041)
+elif country=='taiwan' or country=='TAIWAN':
+    print("name of the country TAIWAN")
+    my_function(8578053,8263503,14890)
+else:
+    print("S00RY not upload...")
+    print("-or-".center(20))
+    print("please chack the spelling!")
+    print("uploading only top 15 corona affected contries..")
+
+
